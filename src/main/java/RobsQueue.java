5f9ddbd6a7b6f3422b65d739be44a1f0b74fab40
Rob Hill <robert.hill0122@gmail.com>
2014-10-09 15:53:14 -0600

    First commit.  Created Queue class

---
+++ src/main/java/RobsQueue.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rob on 10/9/14.
+ */
+public class RobsQueue {
+    public static void main(String[] args) {
+        String[] myQueue = new String[]{"a"};
+//        ArrayList<String> list = new ArrayList<String>();
+        System.out.println(myQueue);
+    }
+}
