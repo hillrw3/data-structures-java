d8efa318a6826ea4dd6986115b7f72e3f4f59f79
Rob Hill <robert.hill0122@gmail.com>
2014-10-09 21:31:25 -0600

    Figuring out methods and if-else statements

--- src/main/java/RobsQueue.java
+++ src/main/java/RobsQueue.java
@@ -1,10 +1,30 @@
 /**
  * Created by Rob on 10/9/14.
  */
+
+import java.util.ArrayList;
+
 public class RobsQueue {
     public static void main(String[] args) {
-        String[] myQueue = new String[]{"a"};
-//        ArrayList<String> list = new ArrayList<String>();
-        System.out.println(myQueue);
+
+        ArrayList<String> myQueue = new ArrayList<String>();
+        boolean empty;
+
+//    void isEmpty(ArrayList<String> list) {
+//        if (list.size() == 0) {
+//            boolean empty = true;
+//        } else {
+//            boolean empty = false;
+//        }
+//    }
+
+        System.out.println("Current queue is: " + myQueue);
+        System.out.println("Current queue length is: " + myQueue.size());
+
+        myQueue.add("Foo");
+        myQueue.add("Bar");
+
+        System.out.println("Current queue is: " + myQueue);
+        System.out.println("Current queue length is: " + myQueue.size());
     }
 }
