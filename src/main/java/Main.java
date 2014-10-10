6496ce6f9d65d244e98dfdae3fa2148cbac51d09
Rob Hill <robert.hill0122@gmail.com>
2014-10-10 11:11:38 -0600

    Created RobsQueue class and added isEmpty and enqueue method

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Rob on 10/9/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        RobsQueue robsQueue = new RobsQueue();
+
+        System.out.println("Robs queue is empty to start: " + robsQueue.isEmpty().toString());
+
+        robsQueue.queue.add("Hello");
+
+        System.out.println("Is the queue empty? " + robsQueue.isEmpty().toString());
+
+        robsQueue.enqueue("Shazam");
+        System.out.println(robsQueue.queue);
+
+
+    }
+}
--- src/main/java/RobsQueue.java
+++ src/main/java/RobsQueue.java
@@ -1,30 +1,22 @@
 /**
- * Created by Rob on 10/9/14.
+ * Created by Rob on 10/10/14.
  */
-
 import java.util.ArrayList;
 
 public class RobsQueue {
-    public static void main(String[] args) {
 
-        ArrayList<String> myQueue = new ArrayList<String>();
-        boolean empty;
+    ArrayList<String> queue = new ArrayList<String>();
 
-//    void isEmpty(ArrayList<String> list) {
-//        if (list.size() == 0) {
-//            boolean empty = true;
-//        } else {
-//            boolean empty = false;
-//        }
-//    }
+    public Boolean isEmpty() {
+        if (queue.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-        System.out.println("Current queue is: " + myQueue);
-        System.out.println("Current queue length is: " + myQueue.size());
-
-        myQueue.add("Foo");
-        myQueue.add("Bar");
-
-        System.out.println("Current queue is: " + myQueue);
-        System.out.println("Current queue length is: " + myQueue.size());
+    public ArrayList enqueue(String item) {
+        queue.add(item);
+        return queue;
     }
 }
